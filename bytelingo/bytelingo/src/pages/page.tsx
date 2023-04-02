49db4a8dff964b9ec055754cb5803fd98b293ea0
amatoria <patran@dons.usfca.edu>
2023-04-01 22:06:59 -0700

    committing before pushing

---
+++ bytelingo/bytelingo/src/pages/page.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-100">
+      <Head>
+        <title>Sign In</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        {/* Log in portion */}
+        <div className="bg-white rounded-2xl shadow-2xl max-w-4xl">
+        <h2>ok</h2>
+        </div>
+    </main>
+    </div>
+  )
+}
