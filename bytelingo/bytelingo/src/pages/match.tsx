706368823228393aa6fd3c803ac0917bc6578508
Haley Lenander <halenander@gmail.com>
2023-04-02 09:00:10 -0700

    matching?

--- bytelingo/bytelingo/src/pages/match.tsx
+++ bytelingo/bytelingo/src/pages/match.tsx
@@ -1,65 +1,74 @@
 import Head from 'next/head'
-import React, {Fragment, useState} from 'react'
+import React, {Fragment, useState, SyntheticEvent} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from './header'
 
 
 export default function Match() {
-  const [left1Color, setLColor1] = useState("btn btn-outline-success btn-block btn-lg")
-  const [left2Color, setLColor2] = useState("btn btn-outline-success btn-block btn-lg")
-  const [left3Color, setLColor3] = useState("btn btn-outline-success btn-block btn-lg")
-  const [left4Color, setLColor4] = useState("btn btn-outline-success btn-block btn-lg")
+  const firstStateG = "btn btn-outline-success btn-block btn-lg"
+  const clickedStateG = "btn btn-success btn-block btn-lg"
+  const firstStateY = "btn btn-outline-warning btn-block btn-lg"
+  const clickedStateY = "btn btn-warning btn-block btn-lg"
+  const [left1Color, setLColor1] = useState(firstStateG)
+  const [left2Color, setLColor2] = useState(firstStateG)
+  const [left3Color, setLColor3] = useState(firstStateG)
+  const [left4Color, setLColor4] = useState(firstStateG)
 
-  const [right1Color, setRColor1] = useState("btn btn-outline-warning btn-block btn-lg")
-  const [right2Color, setRColor2] = useState("btn btn-outline-warning btn-block btn-lg")
-  const [right3Color, setRColor3] = useState("btn btn-outline-warning btn-block btn-lg")
-  const [right4Color, setRColor4] = useState("btn btn-outline-warning btn-block btn-lg")
+  const [right1Color, setRColor1] = useState(firstStateY)
+  const [right2Color, setRColor2] = useState(firstStateY)
+  const [right3Color, setRColor3] = useState(firstStateY)
+  const [right4Color, setRColor4] = useState(firstStateY)
 
   const handleClick = (id: number) => {
+    changeColor(id)
+    submit(id)
+  };
+
+  const changeColor = (id: number) => {
     if (id > 10){
       if (id == 11){
-        setRColor1("btn btn-warning btn-block btn-lg")
+        setRColor1(clickedStateY)
       } else if (id == 12){
-        setRColor2("btn btn-warning btn-block btn-lg")
+        setRColor2(clickedStateY)
       } else if (id == 13){
-        setRColor3("btn btn-warning btn-block btn-lg")
+        setRColor3(clickedStateY)
       } else {
-        setRColor4("btn btn-warning btn-block btn-lg")
+        setRColor4(clickedStateY)
       }
     } else {
       if (id == 1){
-        setLColor1("btn btn-success btn-block btn-lg")
+        setLColor1(clickedStateG)
       } else if (id == 2){
-        setLColor2("btn btn-success btn-block btn-lg")
+        setLColor2(clickedStateG)
       } else if (id == 3){
-        setLColor3("btn btn-success btn-block btn-lg")
+        setLColor3(clickedStateG)
       } else {
-        setLColor4("btn btn-success btn-block btn-lg")
+        setLColor4(clickedStateG)
       }
     }
-    submit(id)
-  };
+  }
   const [lives, setLives] = useState(3);
-  var [left, right] = [-1,-1]
-  var disabled = [false, false, false, false]
+  const [LR, setLR] = useState([-1,-1])
+  const [disabled, setDisabled] = useState([false, false, false, false])
 
   const submit = (id: number) => {
     if (id > 10) {
-      right = (id - 10);
+      LR[1] = (id - 10);
     } else {
-      left = id;
+      LR[0] = id;
     }
-    if (left > 0 && right > 0){
-      if (left == right){
-        disabled[left] = true
+    if (LR[0] > 0 && LR[1] > 0){
+      if (LR[0] == LR[1]){
+        disabled[LR[0]] = true
+        setDisabled(disabled)
       } else {
         const newLives = lives - 1;
         //if lives < 0 game over
         setLives(newLives);
         console.log(newLives);
       }
-      left = -1;
-      right = -1;
+      LR[0] = -1;
+      LR[1] = -1;
     }
   }
   return (
@@ -75,31 +84,23 @@ export default function Match() {
           pick
             <div className= "left-10">
               <div>
-              <button type="button" id="1" name="align" disabled = {disabled[1]} onClick={() => handleClick(1)}/>
-              <label className={left1Color}>if</label>
+              <button className={left1Color} type="button" id="1" name="align" disabled = {disabled[1]} onClick={() => handleClick(1)}>if</button>
             </div><div>
-              <input type="button" id="2" name="align" disabled = {disabled[2]} onClick={() => handleClick(2)}/>
-              <label className={left2Color}>elif</label>
+              <button className={left2Color} type="button" id="2" name="align" disabled = {disabled[2]} onClick={() => handleClick(2)}>elif</button>
             </div><div>
-              <input type="button" id="3" name="align" disabled = {disabled[3]} onClick={() => handleClick(3)}/>
-              <label className={left3Color}>else</label>
+              <button className={left3Color} type="button" id="3" name="align" disabled = {disabled[3]} onClick={() => handleClick(3)}>else</button>
             </div><div>
-              <input type="button" id="4" name="align" disabled = {disabled[4]} onClick={() => handleClick(4)}/>
-              <label className={left4Color}>else</label>
+              <button className={left4Color} type="button" id="4" name="align" disabled = {disabled[4]} onClick={() => handleClick(4)}>else</button>
             </div>
           </div> <div className='right-10'>
             <div>
-              <button type="button" id="11" name="align" disabled = {disabled[1]} onClick={() => handleClick(11)}/>
-              <label className={right1Color}>if</label>
+              <button className={right1Color} type="button" id="11" name="align" disabled = {disabled[1]} onClick={() => handleClick(11)}>else</button>
             </div><div>
-              <input type="button" id="12" name="align" disabled = {disabled[2]} onClick={() => handleClick(12)}/>
-              <label className={right2Color}>elif</label>
+              <button className={right2Color} type="button" id="12" name="align" disabled = {disabled[2]} onClick={() => handleClick(12)}>else</button>
             </div><div>
-              <input type="button" id="13" name="align" disabled = {disabled[3]} onClick={() => handleClick(13)}/>
-              <label className={right3Color}>else</label>
+              <button className={right3Color} type="button" id="13" name="align" disabled = {disabled[3]} onClick={() => handleClick(13)}>else</button>
             </div><div>
-              <input type="button" id="14" name="align" disabled = {disabled[4]} onClick={() => handleClick(14)}/>
-              <label className={right4Color}>else</label>
+              <button className={right4Color} type="button" id="14" name="align" disabled = {disabled[4]} onClick={() => handleClick(14)}>else</button>
             </div>
             </div>
         </h3>
