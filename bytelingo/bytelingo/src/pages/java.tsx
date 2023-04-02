2dea34512ae3f73ae2a080b6722e6c4ec4c75420
lychee <vickiyoung112@gmail.com>
2023-04-02 03:33:38 -0700

    added java and c game level pages

---
+++ bytelingo/bytelingo/src/pages/java.tsx
@@ -0,0 +1,54 @@
+import React, {Fragment, useState} from 'react'
+import Head from 'next/head'
+import Header from './header'
+import 'bootstrap/dist/css/bootstrap.css'
+import layoutStyles from '../styles/Layout.module.css';
+import buttonStyles from '../styles/Button.module.css';
+import messageStyles from '../styles/Message.module.css';
+
+
+export default function Java() {
+    //const name = "ah-ge"
+    //const x = true
+  
+    return (
+      <Fragment>
+      <div className={layoutStyles.container}>
+            <h2>Java Game Levels</h2>
+
+            <div className={messageStyles.success}>
+              Choose a game based on difficulty level.
+            </div>
+            <br />
+
+            <div className="mb-5">
+              <a href="/match">
+              <button className={buttonStyles.easy}>Easy</button>
+              </a>
+            </div>
+
+            <div className="mb-5">
+              <a href="/swap">
+              <button className={buttonStyles.medium}>Medium</button>
+              </a>
+            </div>
+
+            <div className="mb-5">
+              <a href="/game">
+              <button className={buttonStyles.hard}> Hard</button>
+              </a>
+            </div>
+
+            <a href="/">
+            <button className={buttonStyles.button} onClick={() => {
+              console.log("Hello");
+            }}
+            >
+              Home
+            </button>
+            </a>
+      </div>  
+      </Fragment>
+    )
+  }
+  
