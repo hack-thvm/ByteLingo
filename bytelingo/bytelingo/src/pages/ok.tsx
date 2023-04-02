76cf8acc641dcb02ea3c98d874eac217c9c800e1
amatoria <patran@dons.usfca.edu>
2023-04-01 21:37:19 -0700

    front page complete

---
+++ bytelingo/bytelingo/src/pages/ok.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-100">
+    <Head>
+        <title>ByteLingo: Home</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full h-full flex-1 px-20 text-center">
+        {/* Log in portion */}
+        <div className="bg-white text-black w-full py-12 px-12">
+          <div className="py-3">
+ok
+          </div>
+        </div>
+    </main>
+    </div>
+  )
+}
